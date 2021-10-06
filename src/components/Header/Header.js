c7500b8d91e384c3eea917de2fe4abd0bc5a7897
munimrahman <munimrh@gmail.com>
2021-10-07 00:27:51 +0600

    test commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="" />
+            <Link to="/">
+                <img className="logo" src={logo} alt="sdf" />
+            </Link>
             <nav>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
+                <Link to="/shop">Shop</Link>
+                <Link to="/orders">Order Review</Link>
+                <Link to="/inventory">Manage Inventory</Link>
             </nav>
         </div>
     );
---
+++ src/components/ManageInventory/ManageInventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageInventory = () => {
+
+    return (
+        <div>
+            <h1>Manage Inventory Page Coming Soon!</h1>
+        </div>
+    );
+};
+
+export default ManageInventory;
